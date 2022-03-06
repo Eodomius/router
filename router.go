e62876478647f792ef22552f0dbb0f2fa1df5adb
Veauville Raphael <veauville.raphael@gmail.com>
2022-03-06 17:55:09 +0100

    Add other methods

--- router.go
+++ router.go
@@ -25,7 +25,34 @@ func (ro Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 /* Methods functions */
+
+// GET
 func (ro Router) Get(route string, cb func(w http.ResponseWriter, r *http.Request)){
 	ro.routes[route] = cb
 	fmt.Println("GET: " + route)
+}
+
+// POST
+func (ro Router) Post(route string, cb func(w http.ResponseWriter, r *http.Request)){
+	ro.routes[route] = cb
+	fmt.Println("POST: " + route)
+}
+
+// PATCH
+func (ro Router) Patch(route string, cb func(w http.ResponseWriter, r *http.Request)){
+	ro.routes[route] = cb
+	fmt.Println("PATCH: " + route)
+
+}
+
+// PUT
+func (ro Router) Put(route string, cb func(w http.ResponseWriter, r *http.Request)){
+	ro.routes[route] = cb
+	fmt.Println("PUT: " + route)
+}
+
+// DELETE
+func (ro Router) Delete(route string, cb func(w http.ResponseWriter, r *http.Request)){
+	ro.routes[route] = cb
+	fmt.Println("DELETE: " + route)
 }
