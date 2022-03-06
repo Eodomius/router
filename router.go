73ecbb7aaaa11a564440c3c3a62b330a49e8329a
Veauville Raphael <veauville.raphael@gmail.com>
2022-03-06 17:48:07 +0100

    Add router

--- router.go
+++ router.go
@@ -1,5 +1,31 @@
 package router
 
-func Test(a int, b int) int {
-  return a + b;
+import (
+	"fmt"
+	"net/http"
+)
+
+type Router struct {
+	routes map[string]func(w http.ResponseWriter, r *http.Request)
+}
+
+func New()Router{
+	return Router{
+		routes: make(map[string]func(w http.ResponseWriter, r *http.Request)),
+	}
+}
+
+func (ro Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		var route = ro.routes[r.URL.Path]
+		if route != nil {
+			route(w, r)
+		} 
+	}
+}
+
+/* Methods functions */
+func (ro Router) Get(route string, cb func(w http.ResponseWriter, r *http.Request)){
+	ro.routes[route] = cb
+	fmt.Println("GET: " + route)
 }
