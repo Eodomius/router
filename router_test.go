dbf45f89dcb405001664d73d42ba9ed208f78f68
Veauville Raphael <veauville.raphael@gmail.com>
2022-03-09 16:50:17 +0100

    Tests

--- router_test.go
+++ router_test.go
@@ -1,19 +1,20 @@
 package router
 
 import (
+	"io/ioutil"
 	"net/http"
 	"testing"
 )
 
 const PORT = "8080"
 
-func TestRouter(t *testing.T){
+func StartServer(t *testing.T){
 
 	var router = New()
 	http.Handle("/", router)
 	
 	router.Get("/test", func(w http.ResponseWriter, r *http.Request, result *Result){
-		w.Write([]byte("Get 0 "))
+		w.Write([]byte("Get 0"))
 	})
 	router.Get("/test/{id}", func(w http.ResponseWriter, r *http.Request, result *Result){
 		w.Write([]byte("Get 1: " + result.Params["id"]))
@@ -33,5 +34,58 @@ func TestRouter(t *testing.T){
 	router.Delete("/test", func(w http.ResponseWriter, r *http.Request, result *Result){
 		w.Write([]byte("Delete: Test"))
 	})
-	http.ListenAndServe(":"+PORT, nil)
+	http.ListenAndServe("127.0.0.1:"+PORT, nil)
+}
+
+// Test requests for the router
+func TestRequests(t *testing.T){
+	go StartServer(t);
+	TRequest(t, "http://localhost:"+PORT+"/test", "GET", "Get 0")
+	TRequest(t, "http://localhost:"+PORT+"/test/abc", "GET", "Get 1: abc")
+	TRequest(t, "http://localhost:"+PORT+"/test/def/test", "GET", "Get 2: def")
+	TRequest(t, "http://localhost:"+PORT+"/test", "POST", "Post: Test")
+	TRequest(t, "http://localhost:"+PORT+"/test", "PATCH", "Patch: Test")
+	TRequest(t, "http://localhost:"+PORT+"/test", "PUT", "Put: Test")
+	TRequest(t, "http://localhost:"+PORT+"/test", "DELETE", "Delete: Test")
+}
+
+func TRequest(t *testing.T, url, method, exeptedResult string){
+	client := &http.Client{}
+	var (
+		request *http.Request
+		response *http.Response
+		err error
+	)
+	switch method {
+		case "GET":
+		request, _ = http.NewRequest(http.MethodGet, url, nil)
+		response, err = client.Do(request)
+		case "POST":
+		request, _ = http.NewRequest(http.MethodPost, url, nil)
+		response, err = client.Do(request)
+		case "PATCH":
+		request, _ = http.NewRequest(http.MethodPatch, url, nil)
+		response, err = client.Do(request)
+		case "PUT":
+		request, _ = http.NewRequest(http.MethodPut, url, nil)
+		response, err = client.Do(request)
+		case "DELETE":
+		request, _ = http.NewRequest(http.MethodDelete, url, nil)
+		response, err = client.Do(request)
+	}
+
+	if err != nil {
+		t.Error("Error: ", err)
+	}
+	if response.StatusCode != 200 {
+		t.Error("Status code should be 200, but is: ", response.StatusCode)
+	}
+	// Read an check if responce is "Get 0"
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Error("Error: ", err)
+	}
+	if string(body) != exeptedResult {
+		t.Error("Response should be '",exeptedResult,"' but is: ", string(body))
+	}
 }
