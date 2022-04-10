98132166354197e9f2707f3ad072a4621e911a86
Veauville Raphael <veauville.raphael@gmail.com>
2022-04-10 14:36:01 +0200

    Fix comments

--- router.go
+++ router.go
@@ -35,6 +35,7 @@ func (ro Router) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 }
 
 
+/* It's a method that adds a route to the router. */
 func (ro Router) HandleRoute(path, method string, cb func(w http.ResponseWriter, r *http.Request, route *Result)){
 	route := Route{
 		Path: path,
